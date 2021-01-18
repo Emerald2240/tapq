4eb41c0fbc85122c70925f65e56fcdf8f50373e9
Orji Michael <Orji Michael>
2021-01-18 02:58:21 +0100

    made header and footer completely dynamic changed all httml files and its corresponding links to php

---
+++ includes/header.php
@@ -0,0 +1,58 @@
+<div class="header">
+                <div class="container-fluid">
+<div class="header-top row align-items-center">
+                        <div class="col-lg-3">
+                            <div class="brand">
+                                <a href="index.php">
+                                    TAPQ
+                                    <!-- <img src="img/logo.png" alt="Logo"> -->
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-9">
+                            <div class="topbar">
+                                <div class="topbar-col">
+                                    <a href="tel:+234 81 488 638 71"><i class="fa fa-phone-alt"></i>+234 81 488 638 71</a>
+                                </div>
+                                <div class="topbar-col">
+                                    <a href="mailto:Orjimichael4886@gmail.com"><i class="fa fa-envelope"></i>Orjimichael4886@gmail.com</a>
+                                </div>
+                                <div class="topbar-col">
+                                    <div class="topbar-social">
+                                        <a href=""><i class="fab fa-twitter"></i></a>
+                                        <a href=""><i class="fab fa-facebook-f"></i></a>
+                                        <a href=""><i class="fab fa-youtube"></i></a>
+                                        <a href=""><i class="fab fa-instagram"></i></a>
+                                        <a href=""><i class="fab fa-linkedin-in"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="navbar navbar-expand-lg bg-light navbar-light">
+                                <a href="#" class="navbar-brand">MENU</a>
+                                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+
+                                <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                                    <div class="navbar-nav ml-auto">
+                                        <a href="index.php" class="nav-item nav-link active">Home</a>
+                                        <a href="about.php" class="nav-item nav-link">About</a>
+                                        <a href="service.php" class="nav-item nav-link">Service</a>
+                                        <a href="portfolio.php" class="nav-item nav-link">Project</a>
+                                        <a href="single.php" class="nav-item nav-link">Single</a>
+                                        <a href="contact.php" class="nav-item nav-link">Contact</a>
+                                        <div class="nav-item dropdown">
+                                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Dropdown</a>
+                                            <div class="dropdown-menu">
+                                                <a href="#" class="dropdown-item">Sub Item 1</a>
+                                                <a href="#" class="dropdown-item">Sub Item 2</a>
+                                            </div>
+                                        </div>
+                                        <a href="#" class="btn">Get A Quote</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+</div>
